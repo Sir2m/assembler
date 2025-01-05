7d0809f7bc8ef75c3db95f510886b7ff41bc8d22
Mohab <mr.2m0055@gmail.com>
2025-01-05 22:49:56 +0200

    Adding the instruction set

--- assembler.py
+++ assembler.py
@@ -2,7 +2,38 @@ from context import Reader
 import sys
 
 
-DATA = {}
+DATA = {
+    "others": {
+        "CLA": '7800',
+        "CLE": '7400',
+        "CMA": '7200',
+        "CME": '7100',
+        "CIR": '7080',
+        "CIL": '7040',
+        "INC": '7020',
+        "SPA": '7010',
+        "SPN": '7008',
+        "SZA": '7004',
+        "SZE": '7002',
+        "HLT": '7001',
+        "INP": 'F800',
+        "OUT": 'F400',
+        "SKI": 'F200',
+        "SKO": 'F100',
+        "ION": 'F080',
+        "IOF": 'F040'
+    },
+    "mem_ref": {
+        "AND": ["0", "8"],
+        "ADD": ["1", "9"],
+        "LDA": ["2", "A"],
+        "STA": ["3", "B"],
+        "BUN": ["4", "C"],
+        "BSA": ["5", "D"],
+        "ISZ": ["6", "E"]
+    },
+    "labels": {}
+}
 
 def first_pass(instructions: list):
     if instructions[0][0] == "ORG":
@@ -17,7 +48,7 @@ def first_pass(instructions: list):
     
     for i in range(1, len(instructions)):
         if len(instructions[i]) > 2 and instructions[i][0][-1] == ",":
-            DATA[instructions[i][0][:-1]] = i+n
+            DATA["labels"][instructions[i][0][:-1]] = i+n
             instructions[i].pop(0)
 
 
